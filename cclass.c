b7bb642cedcef8e8b56bbb94007bcb2cb9191601
Alrck <arjunal9000@gmail.com>
2024-02-04 23:51:44 +0530

    cclass.c with more examples on class usage

--- cclass.c
+++ cclass.c
@@ -1,36 +1,90 @@
 #include "class.h"
 #include <stdio.h>
-
+#include <string.h>
 int main() {
+	//-------------------Animal Class-------------
+	
     int c = 0;
-
-    // Declare a "Animal" class with variable attributes
+	
+    // Declare class Animal
     DEFINE_CLASS(Animal, int height; double weight;);
 
-    // Declare an object of the "Animal" class and initialize it
-    INITIALIZE_CLASS(Animal, an, 10, 3.14);
-
-    // Access and print object attributes
-    printf("Height: %d\n", an.height);
-    printf("Weight: %f\n", an.weight);
-
-    // Perform the operation
+    // Declare 'an' object
+    
+    
+   	INITIALIZE_CLASS(Animal, an, 10, 3.14);//
+   	//Animal an={10,3.14,"koi"};
+    
+    // Access and print attributes
+    printf("Height:%d\n",an.height);
+    printf("Weight:%f\n",an.weight);
     c = an.height + (int)an.weight;
+    printf("Res:%d\n",c);
 
-    // Print the result
-    printf("Result: %d\n", c);
 
-    // Declare a "Person" class with variable attributes
-    DEFINE_CLASS(Person, char name[50]; int age; float height;);
+	//----------------------Person Class---------------------------//
+    // Declare class Person
+    DEFINE_CLASS(Person, char name[5]; int age; float height;);
 
-    // Declare an object of the "Person" class and initialize it
-    INITIALIZE_CLASS(Person, person, {'J', 'o', 'h', 'n'}, 25, 5.9);
-
+    // Declare a person object
+    Person person={"John", 25, 5.9}; //object allocation 2nd way
+    
+    
     // Access and print object attributes
-    printf("Name: %s\n", person.name);
-    printf("Age: %d\n", person.age);
-    printf("Height: %f\n", person.height);
+    printf("Name:%s\n",person.name);
+    printf("Age:%d\n",person.age);
+    printf("Height:%f\n",person.height);
+    
+    
+    
+	//--------------------Tree Class---------------------------------//
+    //Declare class Tree
+    DEFINE_CLASS(Tree, char name[9]; int width; float height;);
+    
+    //Declare a tr object
+    /*Tree tr={.name="mango",.width=40,.height=621.65};//note this may not supported in old compilers ,also cannot change order of variables
 
+	printf("NAME:%s\n",tr.name);
+	printf("width:%d\n",tr.width);
+	printf("height%d\n",tr.height);
+*/
+
+	//--------------------Car Class---------------------------------
+	//Declare class Car
+	DEFINE_CLASS(Car,char* name;const char *type;int year;char brand[6];);
+	
+	//Declare car object //3rd way initialization
+	Car cr;
+	cr.name=strdup("mustang");
+	cr.type="sport";
+	cr.year=1994;
+	strcpy(cr.brand,"ford"); //require <string.h>
+	
+	printf("Name:%s\n",cr.name);
+	printf("year:%d\n",cr.year);
+	printf("brand:%s\n",cr.brand);
+    
+    
+    //-------------------PC Class-----------------
+    DEFINE_CLASS(PC,const char *name;int year;);
+    PC p2={"dell",2006};
+    
+    printf("name:%s\n",p2.name);
+    printf("year:%d\n",p2.year);
+    p2.year=2003;//this way override values
+    printf("year:%d\n",p2.year);
+    
+    
+	//-------------------Dress Class-------------
+	DEFINE_CLASS(DRESS,const char*type;);
+	DRESS dr;
+	DEFINE_CLASS(DRESS2,DRESS dr;int price;);
+	DRESS2 dr2;
+	dr2.dr.type="overcoat"; //accessing parent class attributes and reassigning them
+	dr2.price=10000;
+	
+	printf("dr2 price:%d\n",dr2.price);
+    
     return 0;
 }
 
--- class.h
+++ class.h
@@ -6,13 +6,13 @@
 #include <stdarg.h>
 #include <stddef.h>
 
-// Define a macro to create a class with variable attributes
+//class using struct
 #define DEFINE_CLASS(class_name, ...) \
     typedef struct { \
         __VA_ARGS__ \
     } class_name
 
-// Function-like macro to initialize an object of a class with variable attributes
+//macro to initialize object
 #define INITIALIZE_CLASS(class_name, obj, ...) \
     class_name obj = { __VA_ARGS__ }
 
